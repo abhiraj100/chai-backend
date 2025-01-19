4f0975545c15338138a3cbe0285dacf641409f4a
Abhiraj Yadav <abhiraj100right@gmail.com>
2025-01-19 05:40:53 +0530

    Logic building Register controller

--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@
 
 import dotenv from "dotenv";
 // dotenv.config();
-import express from "express";
+
 import connectDB from "./db/index.js";
-const app = express();
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB().then(() => {
+connectDB()
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-    })
-}).catch((err) => console.log("MONGODB connection Failed !!!", err));
-
-
-
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("MONGODB connection Failed !!!", err));
 
 // const connectDB = async () => {
 
